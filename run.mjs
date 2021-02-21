96d6bd4e918f17afb55f2e2ad0438cadc353a70b
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-02-21 16:05:40 +0100

    Use Babel

--- run.mjs
+++ run.mjs
@@ -1,11 +1,12 @@
 import { fileURLToPath } from "url";
 import assert from "assert/strict";
 import * as cjs from "cjs-module-lexer";
-import * as esm from "es-module-lexer";
+import { parse } from "@babel/parser";
+import t from "@babel/types";
 import fixturesHelper from "@babel/helper-fixtures";
 const { default: loadFixtures } = fixturesHelper;
 
-await Promise.all([cjs.init(), esm.init]);
+await cjs.init();
 
 const fixturesURL = new URL(
 	"./babel/packages/babel-plugin-transform-modules-commonjs/test/fixtures",
@@ -23,10 +24,14 @@ function compare(fixture, suite) {
 	const name = `${suite.title} / ${fixture.title}`;
 
 	try {
-		const esmExports = normalize(esm.parse(fixture.actual.code)[1]);
-		const cjsExports = normalize(cjs.parse(fixture.expect.code).exports);
+		const cjsResult = cjs.parse(fixture.expect.code);
+		const actual = {
+			exports: normalize(cjsResult.exports),
+			reexports: normalize(cjsResult.reexports),
+		};
+		const expected = babelAnalyze(fixture.actual.code);
 
-		assert.deepEqual(cjsExports, esmExports);
+		assert.deepEqual(actual, expected);
 
 		console.log("OK", name);
 	} catch (e) {
@@ -37,4 +42,44 @@ function compare(fixture, suite) {
 
 function normalize(exports) {
 	return exports.filter((name) => name !== "__esModule").sort();
+}
+
+function babelAnalyze(source) {
+	const exports = [];
+	const reexports = [];
+
+	const ast = parse(source, {
+		sourceType: "module",
+		plugins: [
+			"moduleStringNames",
+			"classProperties",
+			"classPrivateProperties",
+			"classPrivateMethods",
+			"classStaticBlock",
+		],
+	});
+	for (const node of ast.program.body) {
+		if (t.isExportAllDeclaration(node)) {
+			reexports.push(node.source.value);
+		}
+		if (t.isExportDefaultDeclaration(node)) {
+			exports.push("default");
+		}
+		if (t.isExportNamedDeclaration(node)) {
+			for (const specifier of node.specifiers) {
+				exports.push(
+					specifier.exported.name || specifier.exported.value
+				);
+			}
+			if (node.declaration) {
+				exports.push(
+					...Object.keys(t.getOuterBindingIdentifiers(node.declaration))
+				);
+			}
+		}
+	}
+	return {
+		exports: normalize(exports),
+		reexports: normalize(reexports),
+	};
 }
